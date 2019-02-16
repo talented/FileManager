394a21be3347080e62b8d09396231cf29c415a9b
talented <ozguryarikkas@gmail.com>
2019-02-16 20:38:56 +0100

    updated

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 // utils.js
+import moment from 'moment'
 
 // utils to delay promise
 function wait(ms) {
@@ -24,57 +25,41 @@ function sizeFormatter(params) {
 }
 
 function secondsToHMS(secs) {
-  function z(n){return (n<10?'0':'') + n;}
-  var sign = secs < 0? '-':'';
-  secs = Math.abs(secs);
-  return sign + z(secs/3600 |0) + ':' + z((secs%3600) / 60 |0) + ':' + z(secs%60);
+    function z(n){return (n<10?'0':'') + n;}
+    var sign = secs < 0? '-':'';
+    secs = Math.abs(secs);
+    return sign + z(secs/3600 |0) + ':' + z((secs%3600) / 60 |0) + ':' + z(secs%60);
 }
 
 function dateFormatter(params) {
-
     var timestamp = params.value
-    var now = new Date()
     var date = new Date(timestamp)
+    var formattedTime = moment(date).fromNow()
 
-    var time_difference_in_seconds = (now.getTime() - date.getTime()) / 1000
-    // return secondsToHMS(time_difference_in_seconds)
-    const t = 60
-    const times = ['seconds', 'minutes', 'hours', 'days', 'month', 'year']
-    var formattedTime = null
-    switch (true) {
-      case (time_difference_in_seconds < t):
-        formattedTime = (Math.trunc(time_difference_in_seconds)).toString() + " " + times[0]
-        break
-      case (time_difference_in_seconds < 60 * t) :
-        formattedTime = (Math.trunc(time_difference_in_seconds/60)).toString() + " " + times[1]
-        break
-      case (time_difference_in_seconds < 3600 * t):
-        formattedTime = (Math.trunc(time_difference_in_seconds/3600)).toString() + " " + times[2]
-        break
-      case (time_difference_in_seconds < 3600 * 24 * t):
-        formattedTime = (Math.trunc(time_difference_in_seconds/3600 * 24)).toString() + " " + times[3]
-        break
-      case (time_difference_in_seconds < 3600 * 24 * 30 * t):
-        formattedTime = (Math.trunc(time_difference_in_seconds/3600 * 24 * 30)).toString() + " " + times[4]
-        break
-    }
-    return formattedTime + ' ago'
-    // if (time_difference_in_seconds < 60) {
-    //   formattedTime = times[0]
+    // var time_difference_in_seconds = (now.getTime() - date.getTime()) / 1000
+    // // return secondsToHMS(time_difference_in_seconds)
+    // const t = 60
+    // const times = ['seconds', 'minutes', 'hours', 'days', 'month', 'year']
+    // var formattedTime = null
+    // switch (true) {
+    //   case (time_difference_in_seconds < t):
+    //     formattedTime = (Math.trunc(time_difference_in_seconds)).toString() + " " + times[0]
+    //     break
+    //   case (time_difference_in_seconds < 60 * t) :
+    //     formattedTime = (Math.trunc(time_difference_in_seconds/60)).toString() + " " + times[1]
+    //     break
+    //   case (time_difference_in_seconds < 3600 * t):
+    //     formattedTime = (Math.trunc(time_difference_in_seconds/3600)).toString() + " " + times[2]
+    //     break
+    //   case (time_difference_in_seconds < 3600 * 24 * t):
+    //     formattedTime = (Math.trunc(time_difference_in_seconds/3600 * 24)).toString() + " " + times[3]
+    //     break
+    //   case (time_difference_in_seconds < 3600 * 24 * 30 * t):
+    //     formattedTime = (Math.trunc(time_difference_in_seconds/3600 * 24 * 30)).toString() + " " + times[4]
+    //     break
     // }
-    // else if (60 < time_difference_in_seconds < 3600) {
-    //   formattedTime = times[1]
-    // }
-    // else if (3600 < time_difference_in_seconds < 216000) {
-    //   formattedTime = times[2]
-    // }
-    // else if (216000 < time_difference_in_seconds < 12960000) {
-    //   formattedTime = times[3]
-    // }
-    // else if (time_difference_in_seconds > 12960000) {
-    //   formattedTime = times[4]
-    // }
-    //
+    return formattedTime
+
     // return formattedTime + ' ago'
     // // multiplied by 1000 so that the argument is in milliseconds, not seconds.
     // // var date = new Date(timestamp*1000);
